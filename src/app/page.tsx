fa185b70a931590e2be6366cb00b10637c23df03
Anshul Kanwar <4n5hu1@protonmail.com>
2023-06-29 21:20:55 +0530

    update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
+import ComingSoon from "@/components/ui/coming-soon";
+
 export default function Home() {
   return (
-    <main>
-      Hello
+    <main className="min-h-[calc(100vh-8rem)] grid place-items-center">
+      <div className="max-w-4xl space-y-12">
+        <p className="text-3xl font-semibold">
+          Perform document analysis, logo redaction and extract tables from
+          documents in various formats
+        </p>
+        <div className="p-3 grid grid-cols-3 gap-3 bg-slate-100 rounded-lg">
+          <div className="p-5 rounded-md bg-white transition hover:scale-105">
+            <h1 className="mb-5 text-lg font-semibold">Logo Redaction</h1>
+            <p className="text-slate-600">Redact logos from documents</p>
+          </div>
+          <div className="p-5 rounded-md bg-white transition hover:scale-105">
+            <h1 className="mb-5 text-lg font-semibold">Financial Data Extraction</h1>
+            <ComingSoon />
+          </div>
+          <div className="p-5 rounded-md bg-white transition hover:scale-105">
+            <h1 className="mb-5 text-lg font-semibold">Document Analysis</h1>
+            <ComingSoon />
+          </div>
+        </div>
+      </div>
     </main>
-  )
+  );
 }
---
+++ src/components/ui/coming-soon.tsx
@@ -0,0 +1,5 @@
+export default function ComingSoon() {
+  return (
+    <span className="px-2 py-1 rounded-full text-xs font-semibold text-white bg-gradient-to-bl from-rose-500 to-red-300 border-rose-500 border-[1px]">Coming Soon</span>
+  )
+}
