c3bd15c2a0ff42c09ef4f19b8da121023ebd69aa
Anshul Kanwar <4n5hu1@protonmail.com>
2023-06-29 17:41:15 +0530

    add nav

---
+++ src/components/nav.tsx
@@ -0,0 +1,10 @@
+export default function Nav() {
+  return (
+    <nav className="px-10 py-10">
+      <div>
+        <h1 className="text-3xl font-bold">Tablytix</h1>
+      </div>
+      <div></div>
+    </nav>
+  )
+}
