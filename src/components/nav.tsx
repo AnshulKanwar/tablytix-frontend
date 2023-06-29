8bfad40b8f08183b32750c659108cc1f33e46b2e
Anshul Kanwar <4n5hu1@protonmail.com>
2023-06-29 22:16:02 +0530

    add nav link

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,10 +1,27 @@
+import Link from "next/link";
+
+const links = [
+  {
+    name: "Logo Redaction",
+    href: "logo_redaction",
+  },
+];
+
 export default function Nav() {
   return (
-    <nav className="px-10 py-10">
+    <nav className="px-16 py-10 flex justify-between">
       <div>
         <h1 className="text-3xl font-bold">Tablytix</h1>
       </div>
-      <div></div>
+      <div>
+        <ul className="flex">
+          {links.map(({ name, href }) => (
+            <li key={name} className="font-medium text-slate-600 hover:text-slate-800">
+              <Link href={href}>{name}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </nav>
-  )
+  );
 }
